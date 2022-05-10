0353ec71657845f79b790df43e92872570495816
ChristianKareemCianci <ckcianci@gmail.com>
2022-05-10 16:13:36 +0200

    animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('fire', [
+      state(
+        'left',
+        style({
+          transform: 'translateX(0)',
+          position: 'absolute',
+        })
+      ),
+      state(
+        'right',
+        style({
+          transform: 'translateX(200px)',
+          position: 'absolute',
+        })
+      ),
+      transition('right => left', animate(1000)),
+    ]),
+    trigger('jump', [
+      state(
+        'up',
+        style({
+          transform: 'translateY(-50px)',
+          position: 'absolute',
+        })
+      ),
+      state(
+        'down',
+        style({
+          transform: 'translateY(0)',
+          position: 'absolute',
+        })
+      ),
+      transition('down => up', animate(500)),
+      transition('up => down', animate(500)),
+    ]),
+  ],
 })
 export class AppComponent {
-  title = 'dino-game';
+  fireState = 'right';
+  jumpState = 'down';
+
+  onStart() {
+    this.fireState = 'left';
+    setTimeout(() => {
+      this.fireState = 'right';
+    }, 1000);
+    setTimeout(() => {
+      this.onStart();
+    }, 1000);
+  }
+
+  onJump() {
+    this.jumpState = 'up';
+    setTimeout(() => {
+      this.jumpState = 'down';
+    }, 500);
+  }
 }
