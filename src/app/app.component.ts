b54bf467e0ff29214d966ad801efa9ac87d3ad09
kyara <chiara.ko.26@gmail.com>
2022-05-11 15:16:00 +0200

    project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,16 @@ import {
   transition,
   trigger,
 } from '@angular/animations';
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -85,20 +94,47 @@ import { Component } from '@angular/core';
         )
       ),
     ]),
+
+    trigger('melt', [
+      state(
+        'melted',
+        style({
+          transform: 'scale(1.6, 0.2) translateY(50px)',
+        })
+      ),
+      transition('*=> melted', animate(400)),
+    ]),
   ],
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
+  @ViewChild('snowman', { static: true }) snowman: ElementRef;
+  @ViewChild('fire', { static: true }) fire: ElementRef;
+
+  subscription: Subscription = null;
+
+  snowmanPositionX = null;
+  snowmanPositionY = null;
+  firePositionX = null;
+  firePositionY = null;
+
   fireState = 'right';
   jumpState = 'down';
+  meltingState = 'notMelted';
+
+  isAnimationGoing = false;
+  isGameover = false;
+  showingDiv = true;
 
   onStart() {
-    this.fireState = 'left';
-    setTimeout(() => {
-      this.fireState = 'right';
-    }, 1000);
-    setTimeout(() => {
-      this.onStart();
-    }, 1000);
+    this.meltingState = 'notMelted';
+    this.showingDiv = true;
+    this.isGameover = false;
+    this.isAnimationGoing = true;
+    this.animation();
+  }
+
+  onStop() {
+    this.isAnimationGoing = false;
   }
 
   onJump() {
@@ -106,5 +142,69 @@ export class AppComponent {
     setTimeout(() => {
       this.jumpState = 'down';
     }, 300);
+  }
+
+  onMelt() {
+    // si triggera quando si perde
+    this.meltingState = 'melted';
+  }
+
+  onClose() {
+    this.showingDiv = false;
+  }
+
+  private check() {
+    // prendo le posizioni
+
+    const snowmanArray = getComputedStyle(
+      this.snowman.nativeElement
+    ).transform.split(',');
+    this.snowmanPositionY = snowmanArray[5].split(')')[0];
+
+    const fireArray = getComputedStyle(this.fire.nativeElement).transform.split(
+      ','
+    );
+    this.firePositionX = fireArray[4];
+
+    if (
+      135 <= +this.snowmanPositionY + 150 &&
+      150 + +this.snowmanPositionY <= 150 &&
+      50 <= +this.firePositionX &&
+      +this.firePositionX <= 90
+    ) {
+      this.isGameover = true;
+      this.onMelt();
+      setTimeout(() => {
+        this.onStop();
+      }, 400);
+    }
+  }
+
+  //ho spostato l'animazione del fuoco qui perché così si può anche bloccare l'animazione
+  //con la variabile isAnimationGoing
+
+  private animation() {
+    if (this.isAnimationGoing && !this.isGameover) {
+      this.fireState = 'left';
+      setTimeout(() => {
+        this.fireState = 'right';
+      }, 1000);
+      setTimeout(() => {
+        this.subscription.unsubscribe();
+        this.animation();
+      }, 1000);
+      this.subscription = interval(100).subscribe(() => {
+        this.check();
+      });
+    } else {
+      this.subscription.unsubscribe();
+      return;
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
